02dd0298c68793e8d6c05d0bed972ed8d85dd09a
M4RINH0 <dmmartins.dm28@gmail.com>
2025-01-08 11:23:22 -0300

    tela inicial

--- jogo_dos_caminhos/lib/components/home_screen.dart
+++ jogo_dos_caminhos/lib/components/home_screen.dart
@@ -7,11 +7,29 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: GradientBackground(
         child: Center(
-          child: PlayButton(
-            onPressed: () {
-              // Ação quando o botão for pressionado
-              print("Play Button Pressed");
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,  // Centraliza verticalmente
+            crossAxisAlignment: CrossAxisAlignment.center,  // Centraliza horizontalmente
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.black.withOpacity(0.1),  // Cor do ícone e opacidade
+                    size: 300,  // Tamanho do ícone
+                  ),
+                  PlayText(),  // Exibindo o texto "Jogo dos Caminhos"
+                ],
+              ),
+              SizedBox(height: 20),  // Espaço entre o texto e o botão
+              PlayButton(
+                onPressed: () {
+                  // Ação quando o botão for pressionado
+                  print("Play Button Pressed");
+                },
+              ),
+            ],
           ),
         ),
       ),
@@ -29,9 +47,13 @@ class GradientBackground extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Color(0xFF3088BE), Color(0xFF163F58)],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFF3088BE), // Cor inicial
+            Color(0xFF163F58), // Cor final
+          ],
+          stops: [0.3, 1.0], // Define a posição das cores no degradê (30% e 100%)
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
       ),
       child: child,
@@ -52,11 +74,27 @@ class PlayButton extends StatelessWidget {
         shape: CircleBorder(), backgroundColor: Colors.transparent,
         padding: EdgeInsets.all(20),
         shadowColor: Colors.transparent,
+        side: BorderSide(color: Color(0xFFF5B51C), width: 3),  // Borda circular com cor F5B51C
       ),
       child: Icon(
         Icons.play_arrow,
-        color: Colors.white,
+        color: Color(0xFFF5B51C),  // Cor do ícone igual ao texto
         size: 50,
+      ),
+    );
+  }
+}
+
+class PlayText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Jogo\n dos\n Caminhos',
+      textAlign: TextAlign.center,  // Alinhando o texto no centro
+      style: TextStyle(
+        fontFamily: 'Aclonica',  // Usando a fonte Aclonica
+        fontSize: 50,
+        color: Color(0xFFF5B51C),  // Cor do texto F5B51C
       ),
     );
   }
